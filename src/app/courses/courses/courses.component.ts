b749544cb9572c25dadfab04264e828d51fb5a80
RODRIGO Varela <rodrigooliveiravarela@gmail.com>
2021-10-28 21:27:54 -0300

    feat: adicionado material table para listagem

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
+
+  public courses: Course[] = [
+    { id: "1", name: "Angular", category: "front-end" },
+    { id: "2", name: "Angular", category: "front-end" },
+  ];
+  public displayedColumns = ['name', 'category'];
 
   constructor() { }
 
