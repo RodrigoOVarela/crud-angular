4ab82ed03ee0346de0f7abe974753d35b5114134
RODRIGO Varela <rodrigooliveiravarela@gmail.com>
2021-10-31 14:41:09 -0300

    chore: ajuste para tratar erro no retorno da API.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Character } from '../model/character';
 import { Course } from '../model/course';
 import { CoursesService } from './../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-courses',
@@ -18,9 +19,11 @@ export class CoursesComponent implements OnInit {
   // public charactersWithObservable: Observable<Character[]>;
   public displayedColumns = ['name', 'description'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    private snackBar: MatSnackBar) {
     // this.characters = this.coursesService.getMarvelCharactersTap();
-   }
+  }
 
   ngOnInit(): void {
     this.coursesService.getMarvelCharacters()
@@ -30,6 +33,11 @@ export class CoursesComponent implements OnInit {
         console.log("getMarvelCharacters without tap", response.data.results);
       }, err => {
         this.loadedGrid = !this.loadedGrid;
+        this.snackBar.open("Ocorreu um erro no servidor, tente novamente mais tarde", undefined, {
+          duration: 4000,
+          horizontalPosition: 'right',
+          verticalPosition: 'bottom',
+        });
         console.log(err);
       });
   }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -11,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatSnackBarModule,
   ]
 })
 export class AppMaterialModule { }
