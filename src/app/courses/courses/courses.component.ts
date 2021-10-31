4ac9608b3a5b7089f79de5f819a249c4174fdfa9
RODRIGO Varela <rodrigooliveiravarela@gmail.com>
2021-10-31 13:47:48 -0300

    feat: implementado requisição na API marvel e adicionado loading no carregamento de grid.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
-import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import { Character } from '../model/character';
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -10,14 +12,25 @@ import { Course } from '../model/course';
 })
 
 export class CoursesComponent implements OnInit {
+  public loadedGrid: boolean = false;
+  // public courses: Course[] = []
+  public characters: Character[] = [];
+  // public charactersWithObservable: Observable<Character[]>;
+  public displayedColumns = ['name', 'description'];
 
-  public courses: Course[] = []
-  public displayedColumns = ['name', 'category'];
-
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService) {
+    // this.characters = this.coursesService.getMarvelCharactersTap();
+   }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.getAll();
+    this.coursesService.getMarvelCharacters()
+      .subscribe((response: any) => {
+        this.characters = response.data.results;
+        this.loadedGrid = !this.loadedGrid;
+        console.log("getMarvelCharacters without tap", response.data.results);
+      }, err => {
+        this.loadedGrid = !this.loadedGrid;
+        console.log(err);
+      });
   }
-
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
 
+import { Character } from './../model/character';
 import { Course } from './../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  private readonly apiUrl = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=55788f172342a9d35da3427c80eea162&hash=cff54bba4a9ff436b7541cbb693c7eca';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -15,5 +18,22 @@ export class CoursesService {
       { id: "1", name: "Angular", category: "front-end" },
       { id: "2", name: "Angular", category: "front-end" },
     ];
+  }
+
+  public getMarvelCharacters() {
+    return this.httpClient.get<Character[]>(this.apiUrl);
+  }
+
+  //Uso do tap para filtrar os dados
+  public getMarvelCharactersTap() {
+    return this.httpClient.get<Character[]>(this.apiUrl)
+      .pipe(
+        //take = após resposta do servidor finaliza inscrição na origem de dados
+        // take(1),
+
+        //fist = interessado apenas na primeira resposta do servidor
+        first(),
+        tap(characters => console.log("getMarvelCharacters with tap", characters))
+      );
   }
 }
